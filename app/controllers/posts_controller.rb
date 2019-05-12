bf886236d57075a320476fa3b32ff06120665ef1
gbs4ever <gbs4ever2@gmail.com>
2019-05-12 16:57:08 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,15 +20,18 @@ class PostsController < ApplicationController
 
   def edit
   end
-
+ #def body
+  #  post = Post.find(params[:id])
+   # render json: PostSerializer.serialize(post)
+  #end
   def update
     @post.update(post_params)
     redirect_to post_path(@post)
   end
 
   def post_data
-    post = Post.find(params[:id])
-    render plain: post.description
+     post = Post.find(params[:id])
+    render json: PostSerializer.serialize(post)
   end
 
 private
